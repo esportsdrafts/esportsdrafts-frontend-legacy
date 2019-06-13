fbbb530c251b1e1ac1f79f81b55534663140f787
Johan Backman <johan@cultivateai.com>
2019-06-13 13:38:42 -0700

    Fix linter errors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,15 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
 import './registerServiceWorker';
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueAxios, axios);
 
 // TODO: Move into a separate settings module that reads env variables
-axios.defaults.baseURL = "https://api.efantasy.dev"
-
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
+axios.defaults.baseURL = 'https://api.efantasy.dev';
 
 Vue.config.productionTip = false;
 
