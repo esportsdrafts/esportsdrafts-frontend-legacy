93f7f4be63f8602639a29e7fce9166ad82de703f
Johan Backman <johan@cultivateai.com>
2019-07-29 22:11:39 -0700

    Style login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>
-    <h2>Login</h2>
-    <p v-if="$route.query.redirect">You need to login first.</p>
-    <form @submit.prevent="login">
-      <label>
-        <input v-model="username" placeholder="username" />
-      </label>
-      <label>
-        <input v-model="pass" placeholder="password" type="password" />
-      </label> (hint: password1)
-      <br />
-      <button type="submit">login</button>
-      <p v-if="error" class="error">Bad login information</p>
-    </form>
-  </div>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-card class="flat" width="340" color="#1b1b1b" outlined="false">
+        <v-layout align-center justify-center class="mb-8">
+          <v-icon size="52">gamepad</v-icon>
+        </v-layout>
+        <p class="headline font-weight-bold text-uppercase text-center mb-6">Login</p>
+        <v-card-text>
+          <v-form>
+            <v-text-field outlined="true" label="Username" name="username" type="text"></v-text-field>
+            <v-text-field outlined="true" id="password" label="Password" name="password" type="password"></v-text-field>
+            <v-checkbox color="primary" label="Remember me" class="mt-n3"></v-checkbox>
+          </v-form>
+        </v-card-text>
+        <v-card-actions class="pl-4 pr-4">
+          <v-spacer></v-spacer>
+          <v-btn color="primary" block>Login</v-btn>
+        </v-card-actions>
+        <v-layout align-center justify-center class="mb-8">
+          <v-btn flat text color="gray" class="login-custom-link">Forgot username/password?</v-btn>
+        </v-layout>
+        <v-layout align-center justify-center class="mb-8">
+          <v-btn class="ma-2 login-custom-link" flat text>
+            Register your account
+            <v-icon right>arrow_right_alt</v-icon>
+          </v-btn>
+        </v-layout>
+      </v-card>
+  </v-layout>
+  </v-container>
 </template>
+
 
 <script>
 import { mapState } from "vuex";
@@ -50,4 +66,9 @@ export default {
 </script>
 
 <style scoped>
+.login-custom-link {
+  color: rgba(255, 255, 255, 0.7);
+  font-size: 0.75em;
+  text-transform: none;
+}
 </style>
